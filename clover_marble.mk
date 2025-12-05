dd2a9b68b192c491ee38ddf1359797ef575b9b3b
marinoalfonse732-boop <marinoalfonse732@gmail.com>
2025-12-05 21:43:22 +0530

    Update and rename lineage_marble.mk to clover_marble.mk

--- clover_marble.mk
+++ clover_marble.mk
@@ -7,10 +7,10 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common Clover configuration
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := clover_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
